fc8ee581b244b094547163ee106f851869b44721
Shu_W <shu@SHUdeMacBook-Pro.local>
2018-05-19 22:05:59 +0100

    map the light data to globe

--- build/encom-globe.js
+++ build/encom-globe.js
@@ -1,4 +1,6 @@
 
+ // window.ENCOM.Globe = require('./src/OrbitControls.js');
+
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 window.ENCOM = (window.ENCOM || {});
 window.ENCOM.Globe = require('./src/Globe.js');
@@ -8,6 +10,9 @@ window.ENCOM.Globe = require('./src/Globe.js');
 var Point = require('./point');
 
 var _faceCount = 0;
+
+// var OrbitControls = require('./OrbitControls.js');
+
 
 var Face = function(point1, point2, point3, register){
     this.id = _faceCount++;
@@ -234,7 +239,6 @@ Point.prototype.midpoint = function(point, location){
     return this.segment(point, .5);
 }
 
-
 Point.prototype.project = function(radius, percent){
     if(percent == undefined){
         percent = 1.0;
@@ -282,7 +286,6 @@ Point.prototype.getOrderedFaces = function(){
         }
         i++;
     }
-
     return ret;
 }
 
@@ -294,7 +297,6 @@ Point.prototype.findCommonFace = function(other, notThisFace){
             }
         }
     }
-
     return null;
 }
 
@@ -321,9 +323,7 @@ var Tile = function(centerPoint, hexSize){
     this.centerPoint = centerPoint;
     this.faces = centerPoint.getOrderedFaces();
     this.boundary = [];
-
     this.triangles = [];
-
 
     for(var f=0; f< this.faces.length; f++){
         this.boundary.push(this.faces[f].getCentroid().segment(this.centerPoint, hexSize));
@@ -506,7 +506,6 @@ module.exports = Tile;
     function hslToRgb(h, s, l)
     {
         var r, g, b;
-
         if (s == 0)
         {
             r = g = b = l; // achromatic
@@ -557,12 +556,7 @@ module.exports = Tile;
 
     var namedColors = 
     {
-        "aliceblue": [240, 248, 255 ],
-        "antiquewhite": [250, 235, 215 ],
-        "aqua": [0, 255, 255 ],
-        "aquamarine": [127, 255, 212 ],
-        "azure": [240, 255, 255 ],
-        "beige": [245, 245, 220 ],
+    
         "bisque": [255, 228, 196],
         "black": [0, 0, 0],
         "blanchedalmond": [255, 235, 205],
@@ -577,93 +571,12 @@ module.exports = Tile;
         "cornflowerblue": [100, 149, 237],
         "cornsilk": [255, 248, 220],
         "crimson": [220, 20, 60],
-        "cyan": [0, 255, 255],
-        "darkblue": [0, 0, 139],
-        "darkcyan": [0, 139, 139],
-        "darkgoldenrod": [184, 134, 11],
-        "darkgray": [169, 169, 169],
-        "darkgreen": [0, 100, 0],
-        "darkgrey": [169, 169, 169],
-        "darkkhaki": [189, 183, 107],
-        "darkmagenta": [139, 0, 139],
-        "darkolivegreen": [85, 107, 47],
-        "darkorange": [255, 140, 0],
-        "darkorchid": [153, 50, 204],
-        "darkred": [139, 0, 0],
-        "darksalmon": [233, 150, 122],
-        "darkseagreen": [143, 188, 143],
-        "darkslateblue": [72, 61, 139],
-        "darkslategray": [47, 79, 79],
-        "darkslategrey": [47, 79, 79],
-        "darkturquoise": [0, 206, 209],
-        "darkviolet": [148, 0, 211],
-        "deeppink": [255, 20, 147],
-        "deepskyblue": [0, 191, 255],
-        "dimgray": [105, 105, 105],
-        "dimgrey": [105, 105, 105],
-        "dodgerblue": [30, 144, 255],
-        "firebrick": [178, 34, 34],
-        "floralwhite": [255, 250, 240],
-        "forestgreen": [34, 139, 34],
-        "fuchsia": [255, 0, 255],
-        "gainsboro": [220, 220, 220],
         "ghostwhite": [248, 248, 255],
         "gold": [255, 215, 0],
         "goldenrod": [218, 165, 32],
         "gray": [128, 128, 128],
         "green": [0, 128, 0],
-        "greenyellow": [173, 255, 47],
-        "grey": [128, 128, 128],
-        "honeydew": [240, 255, 240],
-        "hotpink": [255, 105, 180],
-        "indianred": [205, 92, 92],
-        "indigo": [75, 0, 130],
-        "ivory": [255, 255, 240],
-        "khaki": [240, 230, 140],
-        "lavender": [230, 230, 250],
-        "lavenderblush": [255, 240, 245],
-        "lawngreen": [124, 252, 0],
-        "lemonchiffon": [255, 250, 205],
-        "lightblue": [173, 216, 230],
-        "lightcoral": [240, 128, 128],
-        "lightcyan": [224, 255, 255],
-        "lightgoldenrodyellow": [250, 250, 210],
-        "lightgray": [211, 211, 211],
-        "lightgreen": [144, 238, 144],
-        "lightgrey": [211, 211, 211],
-        "lightpink": [255, 182, 193],
-        "lightsalmon": [255, 160, 122],
-        "lightseagreen": [32, 178, 170],
-        "lightskyblue": [135, 206, 250],
-        "lightslategray": [119, 136, 153],
-        "lightslategrey": [119, 136, 153],
-        "lightsteelblue": [176, 196, 222],
-        "lightyellow": [255, 255, 224],
-        "lime": [0, 255, 0],
-        "limegreen": [50, 205, 50],
-        "linen": [250, 240, 230],
-        "magenta": [255, 0, 255],
-        "maroon": [128, 0, 0],
-        "mediumaquamarine": [102, 205, 170],
-        "mediumblue": [0, 0, 205],
-        "mediumorchid": [186, 85, 211],
-        "mediumpurple": [147, 112, 216],
-        "mediumseagreen": [60, 179, 113],
-        "mediumslateblue": [123, 104, 238],
-        "mediumspringgreen": [0, 250, 154],
-        "mediumturquoise": [72, 209, 204],
-        "mediumvioletred": [199, 21, 133],
-        "midnightblue": [25, 25, 112],
-        "mintcream": [245, 255, 250],
-        "mistyrose": [255, 228, 225],
-        "moccasin": [255, 228, 181],
         "navajowhite": [255, 222, 173],
-        "navy": [0, 0, 128],
-        "oldlace": [253, 245, 230],
-        "olive": [128, 128, 0],
-        "olivedrab": [107, 142, 35],
-        "orange": [255, 165, 0],
-        "orangered": [255, 69, 0],
         "orchid": [218, 112, 214],
         "palegoldenrod": [238, 232, 170],
         "palegreen": [152, 251, 152],
@@ -673,33 +586,6 @@ module.exports = Tile;
         "peachpuff": [255, 218, 185],
         "peru": [205, 133, 63],
         "pink": [255, 192, 203],
-        "plum": [221, 160, 221],
-        "powderblue": [176, 224, 230],
-        "purple": [128, 0, 128],
-        "red": [255, 0, 0],
-        "rosybrown": [188, 143, 143],
-        "royalblue": [65, 105, 225],
-        "saddlebrown": [139, 69, 19],
-        "salmon": [250, 128, 114],
-        "sandybrown": [244, 164, 96],
-        "seagreen": [46, 139, 87],
-        "seashell": [255, 245, 238],
-        "sienna": [160, 82, 45],
-        "silver": [192, 192, 192],
-        "skyblue": [135, 206, 235],
-        "slateblue": [106, 90, 205],
-        "slategray": [112, 128, 144],
-        "slategrey": [112, 128, 144],
-        "snow": [255, 250, 250],
-        "springgreen": [0, 255, 127],
-        "steelblue": [70, 130, 180],
-        "tan": [210, 180, 140],
-        "teal": [0, 128, 128],
-        "thistle": [216, 191, 216],
-        "tomato": [255, 99, 71],
-        "turquoise": [64, 224, 208],
-        "violet": [238, 130, 238],
-        "wheat": [245, 222, 179],
         "white": [255, 255, 255],
         "whitesmoke": [245, 245, 245],
         "yellow": [255, 255, 0],
@@ -2863,13 +2749,11 @@ var self = self || {};/**
 var THREE = { REVISION: '66' };
 
 self.console = self.console || {
-
 	info: function () {},
 	log: function () {},
 	debug: function () {},
 	warn: function () {},
 	error: function () {}
-
 };
 
 // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
@@ -13287,10 +13171,25 @@ THREE.OrthographicCamera.prototype.updateProjectionMatrix = function () {
 
 };
 
+
+
+
+
 THREE.OrthographicCamera.prototype.clone = function () {
 
 	var camera = new THREE.OrthographicCamera();
     // var controls;
+
+
+
+                    var controls = new THREE.OrbitControls( scene.userData.camera, scene.userData.element );
+                    controls.minDistance = 2;
+                    controls.maxDistance = 5;
+                    controls.enablePan = false;
+                    controls.enableZoom = false;
+                    scene.userData.controls = controls;
+
+
 
 	THREE.Camera.prototype.clone.call( this, camera );
 
@@ -42161,7 +42060,10 @@ Globe.prototype.addPin = function(lat, lon, text){
 }
 
 //potentially add url to this marker
-Globe.prototype.addMarker = function(lat, lon, text, connected){
+// Globe.prototype.addMarker = function(lat, lon, text, connected){
+
+Globe.prototype.addMarker = function(lat, lon, text, url,connected){
+
 
     var marker;
     var opts = {
@@ -42170,11 +42072,11 @@ Globe.prototype.addMarker = function(lat, lon, text, connected){
     };
 
     if(typeof connected == "boolean" && connected){
-        marker = new Marker(lat, lon, text, 1.2, this.markers[this.markers.length-1], this.scene, opts);
+        marker = new Marker(lat, lon, text, 1.2, url,this.markers[this.markers.length-1], this.scene, opts);
     } else if(typeof connected == "object"){
-        marker = new Marker(lat, lon, text, 1.2, connected, this.scene, opts);
+        marker = new Marker(lat, lon, text, 1.2,url, connected, this.scene, opts);
     } else {
-        marker = new Marker(lat, lon, text, 1.2, null, this.scene, opts);
+        marker = new Marker(lat, lon, text, 1.2, url,null, this.scene, opts);
     }
 
     this.markers.push(marker);
@@ -42418,7 +42320,7 @@ var createMarkerTexture = function(markerColor) {
 
 };
 
-var Marker = function(lat, lon, text, altitude, previous, scene, _opts){
+var Marker = function(lat, lon, text, altitude, url,previous, scene, _opts){
 
     /* options that can be passed in */
     var opts = {
@@ -42444,7 +42346,7 @@ var Marker = function(lat, lon, text, altitude, previous, scene, _opts){
     this.lat = parseFloat(lat);
     this.lon = parseFloat(lon);
     this.text = text;
-// this.url=url;
+    this.url=url;
     this.altitude = parseFloat(altitude);
     this.scene = scene;
     this.previous = previous;
